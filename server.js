743659c2fc95b940e5047495c4a150eef346c98a
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-09 22:46:00 -0700

    // Set the view engine to use EJS

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Set the view engine to use EJS
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
